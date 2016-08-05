547bfca43c208da9466814304985913d1c8c66f1
Tim Miller <tim.miller@e-gineering.com>
2016-08-05 11:56:42 -0400

    Lesson 3B Step 17 - fire an email intent with the order info

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -30,7 +32,7 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayMessage(createOrderSummary(calculatePrice(), hasWhippedCream, hasChocolate));
+        createOrderSummary(calculatePrice(), hasWhippedCream, hasChocolate);
     }
 
     /**
@@ -40,22 +42,6 @@ public class MainActivity extends ActionBarActivity {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + quant);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        priceTextView.setText(message);
     }
 
     public void increment(View view) {
@@ -94,16 +80,24 @@ public class MainActivity extends ActionBarActivity {
      *
      * @param price of the total order
      */
-    private String createOrderSummary(int price, boolean whippedCreamCheckBoxState,
+    private void createOrderSummary(int price, boolean whippedCreamCheckBoxState,
                                         boolean chocolateCheckBoxState) {
         EditText nameEditText = (EditText) findViewById(R.id.name_edit_text);
 
-        return  "Name: " + nameEditText.getText().toString() +
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "JustJava order for " + nameEditText.getText() );
+        intent.putExtra(Intent.EXTRA_TEXT, "Name: " + nameEditText.getText().toString() +
                 "\nAdd whipped cream? " + whippedCreamCheckBoxState +
                 "\nAdd chocolate? " + chocolateCheckBoxState +
                 "\nQuantity: " + quantity +
                 "\nTotal: $" + price +
-                "\nThank you!";
+                "\nThank you!");
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+            Log.v(this.getClass().getSimpleName(), "intent sent");
+        }
     }
 
     public void updateCheckBoxState(View view) {
